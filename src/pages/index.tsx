cfbd6d1fc1bbaf7ab644175667096b3e70e0ecf1
Johanna <adrina0002@gmail.com>
2024-02-08 16:56:33 +0100

    add new frame for start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,8 +38,7 @@ export default function Home() {
         <meta property="fc:frame" content="vNext" />
         
         <meta property="fc:frame:image" content={ogImageUrl} />
-        <meta property="fc:frame:input:text" content="Type your answer" />
-        <meta property="fc:frame:button:1" content="Submit ✉️" />
+        <meta property="fc:frame:button:1" content="START YOUR QUIZ" />
         <meta
           property="fc:frame:post_url"
           content={`${SERVER_URL}/api/post?data=start`}
